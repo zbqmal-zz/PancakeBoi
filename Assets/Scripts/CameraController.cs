81befd9673e0c46d07a28f90a28e93951672e9be
tecale <ggnalejandro@gmail.com>
2020-02-01 17:53:41 -0500

    Added better movement and camera control

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    
+    public GameObject target;
+    public float cameraSpeed = 0.3f;
+    public float rotationDelta;
+    private Vector3 offset;
+    private float radius;
+
+    void Start() {
+        offset = this.transform.position - target.transform.position;
+        radius = (new Vector3(offset.x, 0, offset.z)).magnitude;
+    }
+
+    public void Update() {
+        float angle = cameraSpeed*Input.GetAxis("Mouse X");
+
+        
+        if (Input.GetKey("q")) {
+            angle -= rotationDelta;
+        }
+        if (Input.GetKey("e")) {
+            angle += rotationDelta;
+        }
+
+        // float newX = radius*Mathf.Cos(Mathf.Acos(offset.x/radius) + angle);
+        // float newZ = radius*Mathf.Cos(Mathf.Acos(offset.z/radius) + angle);
+
+        // float newX = offset.x*Mathf.Cos(angle) - offset.z*Mathf.Sin(angle);
+        // float newZ = offset.z*Mathf.Cos(angle) - offset.x*Mathf.Sin(angle);
+
+        // float newX = Mathf.Sin(angle + Mathf.Atan(offset.x/offset.z));
+        // float newZ = Mathf.Cos(angle + Mathf.Atan(offset.x/offset.z));
+
+        float newX = Mathf.Sin(angle)*(offset.z) + Mathf.Cos(angle)*(offset.x);
+        float newZ = Mathf.Cos(angle)*(offset.z) - Mathf.Sin(angle)*(offset.x);
+
+        offset.x = newX;
+        offset.z = newZ;
+        // this.transform.position = target.transform.position + offset;
+        this.transform.position = target.transform.TransformPoint(offset);
+    
+        this.transform.LookAt(target.transform.position);
+    }
+
+    
+
+    
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float jumpHeight;
+    public float moveSpeed;
+    public float rotationDelta;
+
+    private float rotationSpeed = 0.3f; 
+    private bool isGrounded = true;
+    void Start() {
+        rb = this.GetComponent(typeof(Rigidbody)) as Rigidbody;
+    }
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        float rotation = Input.GetAxis("Mouse X")*rotationSpeed*180/Mathf.PI;
+
+        if (Input.GetKey("q")) {
+            rotation -= rotationDelta;
+        }
+        if (Input.GetKey("e")) {
+            rotation += rotationDelta;
+        }
+
+        this.transform.Rotate(0,rotation,0);
+
+        h = h * Mathf.Sqrt(1f - 0.5f * v * v);
+        v = v * Mathf.Sqrt(1f - 0.5f * h * h);
+        rb.MovePosition(this.transform.TransformPoint(moveSpeed*h*Time.deltaTime, 0, moveSpeed*v*Time.deltaTime));
+
+        if (rb != null && Input.GetButtonDown("Jump") && isGrounded) {
+            isGrounded = false;
+            rb.velocity += new Vector3(0,jumpHeight,0);
+        }
+
+    }
+
+    void OnCollisionEnter() {
+        isGrounded = true;
+    }
+
+    void OnCollisionExit() {
+        isGrounded = false;
+    }
+
+}
