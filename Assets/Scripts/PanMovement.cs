736e2ad84f3aac1d5e4f0e862c9cc857db6afd15
Marites Hendrix <marites.hendrix@gmail.com>
2020-03-05 22:59:11 -0500

    Added pan prefab and added pan and ingredients to level 2

---
+++ Assets/Scripts/PanMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool appear;
+    void Start()
+    {
+        appear = true;
+        this.gameObject.active = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
