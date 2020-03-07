95d60b49a52ca486f05ee337c4d75b7b488787f4
Marites Hendrix <marites.hendrix@gmail.com>
2020-03-07 13:51:27 -0500

    Hearts added to HUD

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    private float health = 3;
+    private int heartsTotal = 3;
+    public Image[] hearts;
+    public Sprite halfHeart;
+    public Sprite fullHeart;
+
+    public void Update(){
+        //3 hearts total
+        for (int i = 0; i< hearts.Length; i++){
+            //Setting sprite to full or half heart depending on health value
+            if(i < health - .5f){
+                hearts[i].sprite = fullHeart;
+            }else if (i < health){
+                hearts[i].sprite = halfHeart;
+            }
+
+            //enabling or disabling sprites to show or not show. Full heart gone means it won't show a heart
+            if(i < health){
+                hearts[i].enabled = true;
+            }else{
+                hearts[i].enabled = false;
+            }
+        }
+    }
+
+}
