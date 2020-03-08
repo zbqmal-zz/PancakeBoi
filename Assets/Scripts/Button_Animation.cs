b8712b0b7f89fbf2d2223dcddd0a50997351a8d8
Joonyeol Oh <joonohjoon@Joonyeols-MacBook-Pro.local>
2020-03-08 10:34:50 -0400

    Button & MovingWall

---
+++ Assets/Scripts/Button_Animation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_Animation : MonoBehaviour
+{
+    [SerializeField] private Animator buttonController;
+    [SerializeField] private Animator wallController;
+    public bool isPressed = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            buttonController.SetBool("isPressed", true);
+            wallController.SetBool("isOpened", true);
+            isPressed = true;
+    }
+}
