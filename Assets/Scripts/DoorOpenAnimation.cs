6f6673462e6a98ffedd4d1c01c60b2fedcb088bf
Joonyeol Oh <joonohjoon@Joonyeols-MacBook-Pro.local>
2020-04-27 15:16:56 -0400

    Added door sound effects

--- Assets/Scripts/DoorOpenAnimation.cs
+++ Assets/Scripts/DoorOpenAnimation.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class DoorOpenAnimation : MonoBehaviour
 {
     [SerializeField] private Animator doorController;
+    private AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
     
     private void OnTriggerEnter(Collider c)
     {
-		//if (c.CompareTag("Player"))
-		doorController.SetBool("isOpen", true);
-        doorController.SetBool("isClose", false);
+		if (c.CompareTag("Player")) {
+            doorController.SetBool("isOpen", true);
+            doorController.SetBool("isClose", false);
+            audio.Play();
+        }
     }
 
     private void OnTriggerExit(Collider c)
     {
-        //if (c.CompareTag("Player"))
-        doorController.SetBool("isOpen", false);
-        doorController.SetBool("isClose", true);
+        if (c.CompareTag("Player")) {
+            doorController.SetBool("isOpen", false);
+            doorController.SetBool("isClose", true);
+            audio.Play();
+        }
     }
 }
